5960aad5e9415638fffc10f5f3e941f94fad41c1
sudan <sudharson455@gmail.com>
2022-01-31 18:51:04 +0530

    my first commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Admin.DESKTOP-6CSD19K\Desktop\carzone-project\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
